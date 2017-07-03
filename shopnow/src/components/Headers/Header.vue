4e6cade4a2c81f54743b9b9249060fe5d123ffb6
dvir <dvir@edea.co.il>
2017-07-03 16:32:15 +0300

    Adding logo image and fix text center

--- shopnow/src/components/Headers/Header.vue
+++ shopnow/src/components/Headers/Header.vue
@@ -1,12 +1,17 @@
 /* eslint-disable indent */
 <<template>
   <div id='headercart'>
-     <div id="logo"><img src="../../assets/images/cart/cd-logo.svg" alt="Homepage"></div>
+     <!--<div id="logo"><img src="../../assets/images/cart/cd-logo.svg" alt="Homepage"></div>-->
+    <div id="logo"><img class="logoheader" src="http://mobileapi.edea.co.il/mobileresources/RetailChains/castro.png" alt="Homepage"></div>
 
 		<div id="cd-hamburger-menu"><a class="cd-img-replace" href="#0">Menu</a></div>
 		<router-link to="/ShoppingCart">
       <div id="cd-cart-trigger"><a class="cd-img-replace" href="#0" v-on:click="cartTrigger"></a></div>
     </router-link>
+    <div class="item">
+      <span class="textabove headertext">Aloni Ploni</span><br>
+      <span class="textbelow headertext">Subtotal : 100$</span>
+    </div>
     </div>
 </template>
 
@@ -25,6 +30,31 @@ export default {
 
 <style scouped lang='sass'>
 @import '../../common/sass/base.scss'
+
+.headertext
+  color: white
+  text-align: center
+
+div.item 
+    display: inline-block
+    text-align: center
+    width: 30%
+    height: 100%
+    position: relative
+    line-height: 1.5
+    top: 20%
+
+.caption 
+    display: block
+
+.textabove 
+  top: 20%
+  left: 40%
+
+.textbelow
+  top: 50%
+  left: 40%
+
 .cd-img-replace 
   /* replace text with a background-image */
   display: inline-block;
@@ -45,27 +75,56 @@ export default {
   #headercart 
     height: 80px;
   
+.logoheader
+  height: 100%
+  
+
 #logo 
-  padding-top: 5px;
-  -webkit-transform: scale(0.8);
-  -moz-transform: scale(0.8);
-  -ms-transform: scale(0.8);
-  -o-transform: scale(0.8);
-  transform: scale(0.8);
+  float:right
+  -webkit-transform: scale(0.8)
+  -moz-transform: scale(0.8)
+  -ms-transform: scale(0.8)
+  -o-transform: scale(0.8)
+  transform: scale(0.8)
+  background: white
+  height: 100%
 
 @media only screen and (min-width: 1200px) 
   #logo 
-    padding-top: 20px;
+    float:right
     position: absolute;
     /* logo left margin on Dekstop */
-    right: 2.4em;
-    top: 0;
-    -webkit-transform: scale(1);
-    -moz-transform: scale(1);
-    -ms-transform: scale(1);
-    -o-transform: scale(1);
-    transform: scale(1);
+    right: 2.4em
+    -webkit-transform: scale(1)
+    -moz-transform: scale(1)
+    -ms-transform: scale(1)
+    -o-transform: scale(1)
+    transform: scale(0.5)
+     background: white
   
+@media only screen and (min-width: 600px) and (max-width: 1200px) 
+  #logo 
+    float:right
+    position: absolute
+    /* logo left margin on Dekstop */
+    right: 0.4em
+    -webkit-transform: scale(1)
+    -moz-transform: scale(1)
+    -ms-transform: scale(1)
+    -o-transform: scale(1)
+    transform: scale(0.5)
+    background: white
+
+  div.item 
+    display: inline-block;
+    text-align: center;
+    width: 30%;
+    height: 100%;
+    position: relative;
+    line-height: 1.5;
+    top: 5%;
+
+
 #cd-hamburger-menu, #cd-cart-trigger 
   position: absolute;
   top: 0;
