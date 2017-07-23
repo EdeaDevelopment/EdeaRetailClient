d608f713c93f8c8699e66fd209f481e0bd7db75a
aharonedea <ahron@edea.co.il>
2017-07-23 15:43:30 +0300

    promiss top import

--- shopnow/src/main.js
+++ shopnow/src/main.js
@@ -1,3 +1,4 @@
+import 'es6-promise/auto'
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 /* eslint-disable no-unused-vars */
